161ffa8829df3434393f2b6781b9f3f0e9fe200d
shidorn <78392833+shidorn@users.noreply.github.com>
2024-06-26 09:43:41 +0800

    make login a landing page

--- meal-minder/app/page.tsx
+++ meal-minder/app/page.tsx
@@ -1,12 +1,12 @@
-import Image from "next/image";
-import Button from "./components/buttons/button";
+// import Image from "next/image";
+// import Button from "./components/buttons/button";
 import Login from "./(pages)/login/page";
-import Sidebar from "./components/sidebar/page";
+// import Sidebar from "./components/sidebar/page";
 
 export default function Home() {
   return (
     <div>
-      <Sidebar />
+      <Login />
     </div>
   );
 }
