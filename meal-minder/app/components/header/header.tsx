7b26402de02acb8f259524ba0f82958289cfe2e6
Christine <ocanachristine43@gmail.com>
2024-06-28 13:24:23 +0800

    Add dropdown to profile section

--- meal-minder/app/components/header/header.tsx
+++ meal-minder/app/components/header/header.tsx
@@ -10,6 +10,7 @@ import { useRouter } from "next/navigation";
 const Header = () => {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
+  const [isDropDownVisible, setIsDropDownVisible] = useState(false);
 
   const handleLogoutClick = async (e: any) => {
     e.preventDefault();
@@ -23,29 +24,73 @@ const Header = () => {
     router.push("/login");
     // }
   };
+  const handleProfileClick = () => {
+    setIsDropDownVisible(!isDropDownVisible);
+  };
 
   return (
     <header className="w-full h-16 bg-white text-black flex items-center justify-end px-4 shadow-md">
       <div className="flex items-center space-x-4">
-        <Button
+        {/* <Button
           title="Logout"
           onClick={handleLogoutClick}
           disabled={loading}
           loader={<ClipLoader size={24} />}
-        />
+        /> */}
         <button className="flex flex-row gap-2 items-center hover:text-red-900">
           <FaBell />
           <p>Notifications</p>
         </button>
-        <div className="flex items-center space-x-2">
-          <Image
-            src="/images/unnamed.jpg"
-            alt="User Profile"
-            width={32}
-            height={32}
-            className="rounded-full w-fit"
-          />
-          <span className="hidden md:block">User Name</span>
+        {/* Dropdown Menu */}
+        <div className="relative">
+          <div
+            className="flex items-center space-x-2 cursor-pointer"
+            onClick={handleProfileClick}
+          >
+            <Image
+              src="/images/unnamed.jpg"
+              alt="User Profile"
+              width={32}
+              height={32}
+              className="rounded-full w-fit"
+            />
+            <span className="hidden md:block">User Name</span>
+            {/* Arrow for dropdown */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={`h-4 w-4 ml-1 ${
+                isDropDownVisible ? "rotate-180" : ""
+              } text-gray-400 transition-transform duration-300`}
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 12a1 1 0 01-.707-.293l-4-4a1 1 0 111.414-1.414L10 9.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-.707.293z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          {/* Dropdown content */}
+          {isDropDownVisible && (
+            <div className="absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-lg py-1">
+              <a
+                href="#"
+                className="block px-4 py-2 text-gray-800 hover:bg-gray-300"
+              >
+                Profile
+              </a>
+              <hr className="my-1" />
+              <a
+                href="#"
+                className="block px-4 py-2 text-red-900 hover:bg-gray-300"
+                onClick={handleLogoutClick}
+                download={<ClipLoader size={24} />}
+              >
+                Logout
+              </a>
+            </div>
+          )}
         </div>
       </div>
     </header>
