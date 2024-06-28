3e74e5d21c1106b13d08577677afaf77dc9adefb
Christine <ocanachristine43@gmail.com>
2024-06-28 14:00:05 +0800

    updated UI

--- meal-minder/app/components/header/header.tsx
+++ meal-minder/app/components/header/header.tsx
@@ -1,16 +1,36 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FaBell } from "react-icons/fa";
 import Image from "next/image";
 import Button from "@/app/components/buttons/button";
 import { ClipLoader } from "react-spinners";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { GoPersonFill, GoSignOut } from "react-icons/go";
 
 const Header = () => {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
   const [isDropDownVisible, setIsDropDownVisible] = useState(false);
+  const [user, setUser] = useState({
+    name: "",
+    profilePicture: "/images/default-profile.jpg",
+  });
+  useEffect(() => {
+    const fetchUserData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get("/users");
+        setUser(response.data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUserData();
+  }, []);
 
   const handleLogoutClick = async (e: any) => {
     e.preventDefault();
@@ -48,13 +68,13 @@ const Header = () => {
             onClick={handleProfileClick}
           >
             <Image
-              src="/images/unnamed.jpg"
+              src={user.profilePicture}
               alt="User Profile"
               width={32}
               height={32}
               className="rounded-full w-fit"
             />
-            <span className="hidden md:block">User Name</span>
+            <span className="hidden md:block">{user.name}</span>
             {/* Arrow for dropdown */}
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -76,17 +96,19 @@ const Header = () => {
             <div className="absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-lg py-1">
               <a
                 href="#"
-                className="block px-4 py-2 text-gray-800 hover:bg-gray-300"
+                className="flex items-center gap-4 font-medium px-4 py-2 text-gray-800 hover:bg-gray-100"
               >
+                <GoPersonFill />
                 Profile
               </a>
               <hr className="my-1" />
               <a
                 href="#"
-                className="block px-4 py-2 text-red-900 hover:bg-gray-300"
+                className="flex items-center gap-4 font-medium px-4 py-2 text-red-900 hover:bg-gray-100"
                 onClick={handleLogoutClick}
                 download={<ClipLoader size={24} />}
               >
+                <GoSignOut />
                 Logout
               </a>
             </div>
